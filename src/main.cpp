1bbcdfb9e5beb51ccd92d7b54c00f41b98c06a8a
RusProgger <ruslantimka92@gmail.com>
2026-05-29 16:52:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-#include <format>
-
+#include <windows.h>
 
 int main() {
-    
 
     
+    // CPU info
+    SYSTEM_INFO cpuInfo;
+    GetSystemInfo(&cpuInfo);
+    std::cout << "CPU: " << cpuInfo.dwNumberOfProcessors << std::endl;
+    std::cout << "CPU Architecture: ";
+    switch (cpuInfo.wProcessorArchitecture) {
+        case PROCESSOR_ARCHITECTURE_AMD64: std::cout << "x64\n"; break;
+        case PROCESSOR_ARCHITECTURE_INTEL: std::cout << "x86\n"; break;
+        default: std::cout << "Other\n"; break;
+    }
+
+    // Memory info
+    MEMORYSTATUSEX memoryInfo;
+    memoryInfo.dwLength = sizeof(memoryInfo);
+    GlobalMemoryStatusEx(&memoryInfo);
+    std::cout << "Total RAM: " << memoryInfo.ullTotalPhys / (1024 * 1024) << " MB\n";
+
+    // OS Info
+    OSVERSIONINFOEX osVer;
+    ZeroMemory(&osVer, sizeof(OSVERSIONINFOEX));
+    osVer.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+    if (GetVersionEx((OSVERSIONINFO*)&osVer)) {
+        std::cout << "Windows version: " 
+                  << osVer.dwMajorVersion << "." 
+                  << osVer.dwMinorVersion << std::endl;
+    }
+
     return 0;
 }
