b7a5ad3b514da25669b4bd50c360d6f11ba9c715
RusProgger <ruslantimka92@gmail.com>
2026-05-29 20:02:24 +0300

    Update main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
 #include <windows.h>
 #include "../include/cpuModel.h"
+#include "../include/cpuinfo.h"
+#include "../include/memoryInfo.h"
+#include <../include/osinfo.h>
 
 int main() {
 
-     std::cout << "CPU Model: " << getCpuModel() << std::endl;
-
-    // CPU info
-    SYSTEM_INFO cpuInfo;
-    GetSystemInfo(&cpuInfo);
-    std::cout << "CPU: " << cpuInfo.dwNumberOfProcessors << std::endl;
-    std::cout << "CPU Architecture: ";
-    switch (cpuInfo.wProcessorArchitecture) {
-        case PROCESSOR_ARCHITECTURE_AMD64: std::cout << "x64\n"; break;
-        case PROCESSOR_ARCHITECTURE_INTEL: std::cout << "x86\n"; break;
-        default: std::cout << "Other\n"; break;
-    }
-
-    // Memory info
-    MEMORYSTATUSEX memoryInfo;
-    memoryInfo.dwLength = sizeof(memoryInfo);
-    GlobalMemoryStatusEx(&memoryInfo);
-    std::cout << "Total RAM: " << memoryInfo.ullTotalPhys / (1024 * 1024) << " MB\n";
+    // model cpu 
+    std::cout << "CPU Model: " << getCpuModel() << std::endl;
+    // cpu info
+    getCpuInfo();
+    
+    // memory info
+    getMemoryInfo();
 
     // OS Info
-    OSVERSIONINFOEX osVer;
-    ZeroMemory(&osVer, sizeof(OSVERSIONINFOEX));
-    osVer.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-    if (GetVersionEx((OSVERSIONINFO*)&osVer)) {
-        std::cout << "Windows version: " 
-                  << osVer.dwMajorVersion << "." 
-                  << osVer.dwMinorVersion << std::endl;
-    }
+    getOsInfo();
 
     return 0;
 }
