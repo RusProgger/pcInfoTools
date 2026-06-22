db6b31805f0edcee20f10bac454fcf272fe0a8ec
RusProgger <ruslantimka92@gmail.com>
2026-06-22 18:18:55 +0300

    Update file main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,25 @@
 #include "cpuinfo.h"
 #include "memoryInfo.h"
 #include "osinfo.h"
-#include "header.h"
 #include "user.h"
+#include "color.hpp"
 
 int main() {
+    std::cout << "PcInfoTools: ";
+    colorInfo(10);
+    std::cout << "v1.0001\n";
+    colorInfo(7);
 
-    // Output info 
-    getMainHeaderWelcome("PcInfoTools", "31/05/2026", "1.001", "Ruslan Voronenko");
+    std::cout << "Author: ";
+    colorInfo(11);
+    std::cout << "Ruslan Voronenko\n\n";
+    colorInfo(7);
+
     std::cout << "\n\n";
 
-    std::cout << "System Information\n";
+    std::cout << "System Information:\n";
     std::cout << "-------------------------------------------------\n\n";
-
+    
 
     // info user 
     getUser();
